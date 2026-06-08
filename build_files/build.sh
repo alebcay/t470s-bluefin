469b394121a5295faa6ffcc43beecf0c0dfcdda3
Caleb Xu <calebcenter@live.com>
2026-06-07 22:54:14 -0400

    Adjust i915 modules for t470s

--- build_files/build.sh
+++ build_files/build.sh
@@ -34,6 +34,7 @@ kver="$(cd /usr/lib/modules && echo *)"
 depmod -a "${kver}"
 printf 'export DRACUT_NO_XATTR=1\nreproducible=yes\nadd_dracutmodules+=" bootc ostree "\n' | tee /usr/lib/dracut/dracut.conf.d/20-t470s-bluefin-cachyos-kernel.conf
 printf 'force_drivers+=" i915 "\n' | tee /usr/lib/dracut/dracut.conf.d/20-t470s-early-kms.conf
+printf 'options i915 enable_guc=2\noptions i915 enable_psr=1\noptions i915 enable_rc6=7\n' | tee /usr/lib/modprobe.d/t470s-i915.conf
 dracut -vf "/usr/lib/modules/${kver}/initramfs.img" "${kver}"
 
 setsebool -P domain_kernel_load_modules on
